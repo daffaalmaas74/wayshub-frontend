b20f7406da1147c21d136fe6ea4cbb75bd2db3fb
daffaalmaas74 <daffaalmaas74@gmail.com>
2026-09-09 13:19:00 +0700

    configure api.js

---
+++ src/config/api.js
@@ -0,0 +1,18 @@
+import axios from 'axios';
+
+const API = axios.create({
+    baseURL: "http://api.daffaalmaas.studentdumbways.my.id/api/v1"
+});
+
+const setAuthToken = (token) => {
+    if(token){
+        API.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+    } else {
+        delete API.defaults.headers.common['Authorization'];
+    }
+}
+
+export {
+    API,
+    setAuthToken
+}
